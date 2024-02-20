f1e2b1474b5cd1a27595e52e59a8819791d7e436
FrancoDavid <franco.avalos@live.com>
2024-02-20 15:59:23 -0300

    feat: component header todo

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import TodoHeader from './components/todo-header/todo-header';
 
 function App() {
   return (
@@ -7,17 +8,15 @@ function App() {
       <section className='App-container--bottom'></section>
 
       <aside className='App-container--middle'>
-          <section>
-              <h1>TODO</h1>
-          </section>
-          <section>
+          <TodoHeader />
+          {/* <section>
               <h1>INPUT</h1>
               <h1>TABLA</h1>
               <h1>OPTIONES</h1>
           </section>
           <section>
             <h1>FOOTER</h1>
-          </section>
+          </section> */}
       </aside>
     </main>
   );
---
+++ todo/src/components/todo-header/todo-header.jsx
@@ -0,0 +1,12 @@
+import './todo-header.css';
+
+const TodoHeader = () => {
+    return (
+        <header>
+            <h1>TODO</h1>
+            <span>L/O</span>
+        </header>
+    );
+}
+
+export default TodoHeader;
