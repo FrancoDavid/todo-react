6064f146b35c992836d9ced5db62ef40ae50aea2
FrancoDavid <franco.avalos@live.com>
2024-02-19 22:41:21 -0300

    feat: layout init

---
+++ todo/src/App.js
@@ -0,0 +1,26 @@
+import './App.css';
+
+function App() {
+  return (
+    <main>
+      <section className='App-container--top'></section>
+      <section className='App-container--bottom'></section>
+
+      <aside className='App-container--middle'>
+          <section>
+              <h1>TODO</h1>
+          </section>
+          <section>
+              <h1>INPUT</h1>
+              <h1>TABLA</h1>
+              <h1>OPTIONES</h1>
+          </section>
+          <section>
+            <h1>FOOTER</h1>
+          </section>
+      </aside>
+    </main>
+  );
+}
+
+export default App;
